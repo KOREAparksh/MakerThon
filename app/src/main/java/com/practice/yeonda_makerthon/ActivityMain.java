d806e980f61d2c62ce4ce7718dcdfebde04deb42
KOREAparksh <45682868+KOREAparksh@users.noreply.github.com>
2020-11-16 22:49:53 +0900

    [test]test for json parsing

--- app/src/main/java/com/practice/yeonda_makerthon/ActivityMain.java
+++ app/src/main/java/com/practice/yeonda_makerthon/ActivityMain.java
@@ -8,12 +8,21 @@ import androidx.fragment.app.FragmentTransaction;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ActivityMain extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
@@ -25,6 +34,9 @@ public class ActivityMain extends AppCompatActivity implements BottomNavigationV
     private FragmentSmartKey fragmentSmartKey;
     private FragmentSetting fragmentSetting;
 
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +44,43 @@ public class ActivityMain extends AppCompatActivity implements BottomNavigationV
         setContentView(R.layout.activity_main);
 
         initView();
+        getData();
 
         navi.setOnNavigationItemSelectedListener(this);
         setFrag(1);
 
+    }
+
+    private void getData() {
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String str = snapshot.getValue().toString();
+                Log.d("aaaa","getValue : "+snapshot.getValue());
+                Log.d("aaaa","getValue toString: "+snapshot.getValue().toString());
+                Log.d("aaaa","getKey : "+snapshot.getKey());
+                Log.d("aaaa","getChildren : "+snapshot.getChildren());
+                Log.d("aaaa","getChildrenCount : "+snapshot.getChildrenCount());
+                Log.d("aaaa","getRef : "+snapshot.getRef());
+
+                String cc = "";
+                try {
+                    JSONObject json = new JSONObject(str);
+                    cc = json.get("cc").toString();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                Log.d("aaaa","test : "+str);
+                Log.d("aaaa","test : "+cc);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     private void initView() {
@@ -44,6 +89,9 @@ public class ActivityMain extends AppCompatActivity implements BottomNavigationV
         fragmentReservation = new FragmentReservation();
         fragmentSmartKey = new FragmentSmartKey();
         fragmentSetting = new FragmentSetting();
+
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("spaces");
 
 
         navi.setSelectedItemId(R.id.key);//초반 픽되어있을 곳
